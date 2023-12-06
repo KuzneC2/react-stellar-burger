4b55391faf31963a9e6ad6bd5f1080ae1f38d29e
Dmitriy Kuznecow <grbi.taban@mail.ru>
2023-12-06 14:01:15 +0300

    Сохранил репозитории

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,7 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
 
-function App() {
+function  App() {
   return (
     <div className={styles.app}>
       <pre style={{

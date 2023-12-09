443a90783fb2f771812c1b71f1aea34d622638b5
Dmitriy Kuznecow <grbi.taban@mail.ru>
2023-12-10 01:30:58 +0300

    burger-constructor

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,13 +2,15 @@ import styles from "./app.module.css";
 import AppHeader from "../header/header.jsx";
 import { data } from "../../utils/data";
 import BurgerIngredients from "../ingredients/ingredients";
+import BurgerConstructor from "../burger-constructor/burger-constructor";
 
 function App() {
   return (
     <div className={styles.app}>
       <AppHeader />
       <main className={`${styles.main}`}>
-      <BurgerIngredients data={data} />
+        <BurgerIngredients data={data} />
+        <BurgerConstructor data={data} />
       </main>
     </div>
   );
---
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -0,0 +1,70 @@
+import React, { useMemo } from "react"
+import styles from './burger-constructor.module.css'
+import { ConstructorElement, DragIcon, CurrencyIcon, Button } from "@ya.praktikum/react-developer-burger-ui-components"
+import PropTypes from "prop-types";
+
+function BurgerConstructor({ data }) {
+    const { bun, ingredients } = useMemo(() => ({
+        bun: data.find(item => item.type === 'bun'),
+        ingredients: data.filter(item => item.type !== 'bun')
+    }), [data])
+
+
+    return (
+        <section className={`${styles.listSection} mt-25 ml-10`}>
+            <div className={`${styles.list} ml-4`}>
+                <div className={'pl-8'}>
+                    <ConstructorElement
+                        text={`${bun.name} (верх)`}
+                        type={"top"}
+                        price={bun.price}
+                        thumbnail={bun.image}
+                        isLocked={true}>
+                    </ConstructorElement>
+                </div>
+
+                <div className={`${styles.unblockIngredients}`}>
+                    {ingredients.map(item => (
+                        <div key={item._id} className={styles.card}>
+                            <DragIcon type="primery" />
+                            <ConstructorElement
+                                text={item.name}
+                                price={item.price}
+                                thumbnail={item.image_mobile}
+                            />
+                        </div>
+                    ))
+                    }
+                </div>
+
+                <div className={`ml-4 pl-4`} >
+                    <ConstructorElement
+                        text={`${bun.name} (низ)`}
+                        type={"bottom"}
+                        price={bun.price}
+                        thumbnail={bun.image}
+                        isLocked={true}>
+                    </ConstructorElement>
+                </div>
+
+                <div className={`mt-5 ${styles.total} pr-8`}>
+                    <div className={`${styles.price} mr-10`}>
+                        <p className={"text text_type_digits-medium"}>610</p>
+                        <CurrencyIcon type="primary" />
+                    </div>
+                    <Button htmlType="submit" type="primary" size="large">
+                        Оформить заказ
+                    </Button>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+// BurgerConstructor.propTypes = {
+//     data: PropTypes.arrayOf(ingredientPropType.isRequired).isRequired
+// };
+
+
+export default BurgerConstructor
